c2de45e2ede7b2fa14ac71ad95d540c5db6dad07
Marveric-18 <marvenparmar@gmail.com>
2025-02-25 22:46:30 -0500

    Profile Header Reverted

--- src/components/ProfileHeader/index.js
+++ src/components/ProfileHeader/index.js
@@ -7,7 +7,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import EmailIcon from '@mui/icons-material/Email';
 import ArticleIcon from '@mui/icons-material/Article';
 import IconButton from '@mui/material/IconButton';
-import MarvenProfileHeader from "../../assets/images/HeadShot.png";
+import MarvenProfileHeader from "../../assets/images/MarvenProfileHeader copy.jpeg";
 import MavrenResumePDF from '../../assets/files/MavrenResumePDF.pdf';
 import styles from "./index.module.css";
 
